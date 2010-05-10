5e8e870c91ef2e8b927c5c2e9ddb56485f0e87f4
Actualeyes <actualeyes@gmail.com>
2010-05-10 10:52:34 -0700

    Added network interfaces to xmobar and changed the template to add colors Added names to my workspaces and failed an attempt to automatically start Firefox And Thunderbird in their own workspaces.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,12 +9,15 @@ import System.IO
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Mplayer"   --> doFloat
+    , className =? "Firefox" --> doShift "web"
+    , className =? "Thunderbird" --> doShift "email"
     ]
 
 
 main = do
 	xmproc <- spawnPipe "/usr/local/bin/xmobar /home/apallatto/.xmobarrc"
 	xmonad $ defaultConfig { 
+                workspaces = ["1:web","2:mail","3:term","4:emacs","5:docs","6","7","8","9:pidgin"],
 		manageHook = manageDocks <+> manageHook defaultConfig,
 		layoutHook = avoidStruts  $  layoutHook defaultConfig,
 		logHook = dynamicLogWithPP $ xmobarPP { 
