df41b42aeaf8fbef04b70dabdadc9cb6b6e023e8
Actualeyes <actualeyes@gmail.com>
2010-08-19 00:56:13 -0700

    1) Removed background option for X 2) Customized layouts for Xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import qualified XMonad.StackSet as W
@@ -10,13 +12,40 @@ import System.IO
 myManageHook = composeAll
     [ className =? "Gimp"          --> doFloat
     , className =? "Mplayer"       --> doFloat
-    , className =? "Firefox"       --> doF  (W.shift "1:web"  )
-    , className =? "Thunderbird"   --> doF  (W.shift "2:mail" )
-    , className =? "Mplayer"       --> doF  (W.shift "7:movie")
-    , className =? "Transmission"  --> doF  (W.shift "8:bt"   )
+    , className =? "Firefox"       --> doF  (W.shift "web"  )
+    , className =? "Thunderbird"   --> doF  (W.shift "mail" )
+    , className =? "Mplayer"       --> doF  (W.shift "movie")
+    , className =? "Transmission"  --> doF  (W.shift "bt"   )
     ]
 
+myWide = Mirror $ Tall nmaster delta ratio
+    where
+        -- The default number of windows in the master pane
+        nmaster = 1
+        -- Percent of screen to increment by when resizing panes
+        delta   = 3/100
+        -- Default proportion of screen occupied by master pane
+        ratio   = 80/100
 
+myTall = Tall nmaster delta ratio
+    where
+        -- The default number of windows in the master pane
+        nmaster = 1
+        -- Percent of screen to increment by when resizing panes
+        delta   = 3/100
+        -- Default proportion of screen occupied by master pane
+        ratio   = 80/100
+
+
+
+myWebLayout = noBorders Full ||| myWide 
+myTermLayout = noBorders Full ||| myTall
+myDefLayout = noBorders Full ||| myTall ||| myWide 
+
+mylayoutHook = onWorkspace "web" myWebLayout$
+               onWorkspace "term" myTermLayout
+	       myDefLayout
+	       
 
 
 
@@ -29,9 +58,9 @@ startup = do
 main = do
 	xmproc <- spawnPipe "/usr/local/bin/xmobar /home/apallatto/.xmobarrc"
 	xmonad $ defaultConfig { 
-                workspaces = ["1:web","2:mail","3:term","4:emacs","5:docs","6","7:movie","8:bt","9:im"],
+                workspaces = ["web","mail","term","emacs","docs","6","movie","bt","im"],
 		manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig,
-		layoutHook = avoidStruts  $  layoutHook defaultConfig,
+		layoutHook = avoidStruts $ mylayoutHook,
                 startupHook = startup,
 		logHook = dynamicLogWithPP $ xmobarPP { 
 			ppOutput = hPutStrLn xmproc,
--- lib/X11/xstartup.sh
+++ lib/X11/xstartup.sh
@@ -4,10 +4,7 @@ export GTK_IM_MODULE="ibus"
 trayer --edge top --align right --SetDockType true --SetPartialStrut true \
  --expand true --width 8 --transparent true --tint 0x191970 --height 12 &
  
-# Set the background color
- 
-xsetroot -solid midnightblue
- 
+
 # Fire up appletts 
 xscreensaver -no-splash &
 
