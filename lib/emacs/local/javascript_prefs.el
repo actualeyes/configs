e896927db997aa14090bb0416a3f252669b6bf6e
Anthony Pallatto <actualeyes@gmail.com>
2022-11-19 06:22:33 -0600

    updating javascript to run lsp

--- lib/emacs/local/javascript_prefs.el
+++ lib/emacs/local/javascript_prefs.el
@@ -29,48 +29,21 @@
 ;; Put this file into your load-path and the following into your ~/.emacs:
 ;;   (require 'javascript_prefs)
 
-;;; Code:
+;;; Code:s
 
 (eval-when-compile
   (require 'cl))
 
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (require 'js2-mode)
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;; Better imenu
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
-
-(require 'js2-refactor)
-(require 'xref-js2)
-
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
-(js2r-add-keybindings-with-prefix "C-c C-r")
-(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
-
-;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-;; unbind it.
-(define-key js-mode-map (kbd "M-.") nil)
-
-(add-hook 'js2-mode-hook (lambda ()
-  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+(require 'lsp-mode)
+(add-hook 'js-mode-hook #'lsp)
+(lsp-treemacs-sync-mode 1)
+(define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol)
 
 
-(require 'company)
-(require 'company-tern)
-
-(add-to-list 'company-backends 'company-tern)
-(add-hook 'js2-mode-hook (lambda ()
-                           (tern-mode)
-                           (company-mode)))
-
-;; Disable completion keybindings, as we use xref-js2 instead
-(define-key tern-mode-keymap (kbd "M-.") nil)
-(define-key tern-mode-keymap (kbd "M-,") nil)
-
-
-(setq js-indent-level 2)
-
+;; Typescript
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
